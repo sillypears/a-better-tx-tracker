0f414cb6db92baa8fe391ea353cda80b61aa6e7b
sillypears <sillypears@users.noreply.github.com>
2023-04-01 20:08:09 +0000

    Cleaned up lol

--- project/database.py
+++ project/database.py
@@ -7,6 +7,9 @@ from sqlalchemy_utils import database_exists, create_database
 from flask import current_app
 
 from .models.entry import Entry
+from .models.sale import Sale
+from .models.maker import Maker
+from .models.vendor import Vendor
 from .extentions import db
 
 def connect_db(conf: object):
@@ -18,31 +21,31 @@ def connect_db(conf: object):
 
 def get_next_id(id: int):
   control = {"results": {"prev": -1, "next": -1}} 
-  prev_item = models.Entry.query.order_by(models.Entry.purchase_date.desc()).filter(models.Entry.id < id).first()
-  next_item = models.Entry.query.order_by(models.Entry.purchase_date.asc()).filter(models.Entry.id > id).first()
+  prev_item = Entry.query.order_by(Entry.purchase_date.desc()).filter(Entry.id < id).first()
+  next_item = Entry.query.order_by(Entry.purchase_date.asc()).filter(Entry.id > id).first()
   print(next_item)
   if prev_item is not None and prev_item.id > 0: control["results"]["prev"] = prev_item.id
   if next_item is not None and next_item.id > 0: control["results"]["next"] = next_item.id
   return control
   
 def get_sales():
-  return models.Sale.query.order_by(models.Sale.name).all()
+  return Sale.query.order_by(Sale.name).all()
 
 def get_makers():
-  return models.Maker.query.order_by(models.Maker.name).all()
+  return Maker.query.order_by(Maker.name).all()
 
 def get_vendors():
-  return models.Vendor.query.order_by(models.Vendor.name).all()
+  return Vendor.query.order_by(Vendor.name).all()
 
 def get_entries():
-  return models.Entry.query.order_by(models.Entry.purchase_date.desc()).all()
+  return Entry.query.order_by(Entry.purchase_date.desc()).all()
 
 def get_entries_paginated(limit, offset):
   print(offset, limit)
-  return models.Entry.query.order_by(models.Entry.purchase_date.desc()).offset(offset).limit(limit).all()
+  return Entry.query.order_by(Entry.purchase_date.desc()).offset(offset).limit(limit).all()
 
 def get_entries_total():
-  return models.Entry.query.count()
+  return Entry.query.count()
 
 def get_entry(needle_id: int):
   return Entry.query.filter_by(id=needle_id).one()
